02afb62bfbca13e847057581004e0fa890a0de04
LelsersLasers <LelsersLasers@users.noreply.github.com>
2026-04-29 19:56:36 +0000

    deploy: 1e08d7e17437cd6a5c3745ee67ffd10901bf9ca9

--- trait.impl/core/default/trait.Default.js
+++ trait.impl/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["can_decode",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"can_decode/struct.Parser.html\" title=\"struct can_decode::Parser\">Parser</a>",0]]]]);
+    const implementors = Object.fromEntries([["can_decode",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"can_decode/struct.Parser.html\" title=\"struct can_decode::Parser\">Parser</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
