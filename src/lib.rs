7dfd6a3baa9c9d7f9f11ad6eee598de636b18900
LelsersLasers <millankumar@gmail.com>
2026-04-29 16:24:11 -0400

    clippy lint fix

--- src/lib.rs
+++ src/lib.rs
@@ -571,7 +571,7 @@ impl Parser {
                     }
                 };
                 if let Some(float_value) = float_value {
-                    let scaled_value = float_value as f64 * signal_def.factor + signal_def.offset;
+                    let scaled_value = float_value * signal_def.factor + signal_def.offset;
                     return Some(DecodedSignal {
                         name: signal_def.name.clone(),
                         value: DecodedSignalValue::Numeric(scaled_value),
